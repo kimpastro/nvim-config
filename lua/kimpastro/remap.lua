1d85abfbd4d2f8d3ad4945117069f0becdf1e1b7
Kim Pastro <kimpastro@gmail.com>
2024-09-24 17:49:34 -0300

    remaped netwr to nvim-tree

--- lua/kimpastro/remap.lua
+++ lua/kimpastro/remap.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 -- Opens :Ex the beautiful netwr
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeOpen)
 
 -- Moves the selection
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
